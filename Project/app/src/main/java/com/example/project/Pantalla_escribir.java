43136a9e5849ca4c2e1def2aeda07ba7fa929e3a
Unknown <jokermanjrj@gmail.com>
2019-11-23 15:36:44 +0100

    0.1.4

---
+++ Project/app/src/main/java/com/example/project/Pantalla_escribir.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla_escribir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_escribir);
+    }
+}
