4e6e0f34f3a4d56a1bf30188def7d3f5a18e3aa4
Unknown <jokermanjrj@gmail.com>
2019-11-23 21:13:51 +0100

    0.1.0

--- Application/app/src/main/java/com/example/application/MainActivity.java
+++ Application/app/src/main/java/com/example/application/MainActivity.java
@@ -2,13 +2,64 @@ package com.example.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public MediaPlayer mp;
+    public Button playButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playButton = (Button) findViewById(R.id.button);
+        musica_iniciar();
+        contador();
+        playButton.setVisibility(View.GONE);
+    }
+
+    public void cambio(View view) {
+        Intent intent = new Intent(this,Nombres.class);
+        startActivity(intent);
+    }
+
+
+    public void musica_iniciar()
+    {
+        mp = MediaPlayer.create(this,R.raw.casino);
+        mp.start();
+    }
+
+    public void musica_parar()
+    {
+        mp.stop();
+    }
+
+    public void contador()
+    {
+        new CountDownTimer(5000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                TextView texto_contador = (TextView) findViewById(R.id.contador);
+                texto_contador.setText(""+millisUntilFinished / 1000);
+
+            }
+
+            public void onFinish() {
+                TextView texto_contador = (TextView) findViewById(R.id.contador);
+                texto_contador.setVisibility(View.GONE);
+                playButton.setVisibility(View.VISIBLE);
+                musica_parar();
+            }
+        }.start();
+
     }
 }
---
+++ Application/app/src/main/java/com/example/application/Nombres.java
@@ -0,0 +1,16 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Nombres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nombres);
+    }
+
+
+}
